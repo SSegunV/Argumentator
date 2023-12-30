3409d7a69be16b2c8ea9125bd3e82d789522a312
SSegunV <ovcsinclair@gmail.com>
2023-12-30 14:42:54 +0100

    Initial Commit

---
+++ Argumentator/ArgumentatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArgumentatorApp.swift
+//  Argumentator
+//
+//  Created by Segun Sinclair on 30/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ArgumentatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
